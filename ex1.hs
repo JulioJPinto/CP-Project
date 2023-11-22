e0238f403b2c996080a804c628c6d70d058cb878
Júlio Pinto <pinto.julioj03@gmail.com>
2023-11-22 14:35:35 +0000

    docs: add

--- ex1.hs
+++ ex1.hs
@@ -3,8 +3,10 @@ import Cp
 import Nat
 import List
 
+# pointwise
 ex1 :: [[a]] -> [a]
 ex1 [] = []
 ex1 (h:t) = h ++ ex1(transpose (map (reverse) t))
 
+# pointfree 
 ex1f = (either nil conc) . recList (ex1f . transpose . (map reverse)) . outList
