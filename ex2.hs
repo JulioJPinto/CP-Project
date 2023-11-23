8d27d68bb1aee85231c644d3d123d5294c022986
Júlio Pinto <pinto.julioj03@gmail.com>
2023-11-23 11:50:52 +0000

    feat: add pointwiser ex2

--- ex2.hs
+++ ex2.hs
@@ -7,6 +7,9 @@ import List
 isVowel :: Char -> Bool
 isVowel a = elem a "aeiouAEIOU"
 
+isPar :: Int -> Bool
+isPar a = (mod a 2) == 0
+
 replaceWhen :: (a -> Bool) -> [a] -> [a] -> [a]
 replaceWhen _ l1 [] = l1
 replaceWhen f l1@(h1:t1) l2@(h2:t2) = 
@@ -23,4 +26,15 @@ ex2 f l = replaceWhen f l ((reverse . (filter f)) l1)
 -- pointfree 
 -- replaceWhenf f = A DEFINIR
 
-ex2f f = (uncurry(replaceWhen f)) . (split id (reverse . (filter f) . id))
+replaceWhenf :: (a -> Bool) -> ([a], [a]) -> [a]
+replaceWhenf _ (l1, []) = l1
+replaceWhenf f (l1@(h1:t1), l2@(h2:t2)) = 
+    if f h1 == True then
+        h2:(replaceWhenf f (t1, t2)) 
+    else 
+        h1:(replaceWhenf f (t1, l2))
+
+
+ex2f f = (uncurry(replaceWhen f)) . (split id (reverse . (filter f)))
+
+ex2ff = curry(uncurry(replaceWhenf) . (split p1 (split p2 (reverse. uncurry(filter)))))
