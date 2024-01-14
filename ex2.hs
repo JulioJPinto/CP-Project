232b78bfb023d56df5dd37a07e10d96d40f4f745
Júlio Pinto <pinto.julioj03@gmail.com>
2024-01-14 16:14:41 +0000

    wip: ex4

--- ex2.hs
+++ ex2.hs
@@ -4,7 +4,6 @@ import Distribution.Simple.Utils
 import Cp
 import Nat
 import List
--- Utils ---------------------------
 
 -- isVowel
 isVowel :: Char -> Bool
@@ -41,20 +40,12 @@ replaceWhent f (h1:t1, h2:t2) =
         h1:(replaceWhent f (t1, h2:t2))
 replaceWhent _ (l1, _) = l1
 
--- pointfree replacewhen touples
--- replaceWhent :: (a -> Bool) -> ([a], [a]) -> [a]
--- (replaceWhent f).(id >< nil)  = p1
--- (replaceWhent f).(cons >< cons) = cond (f . p1 . p1) (cons . ((p1 . p2) >< ((replaceWhent f) . ((p2 . p1) >< (p2 . p2))))) (cons . ((p1 . p1) >< ((replaceWhent f) . ((p2 . p1) >< p2))))
-
-
-
 -- End Utils -----------------------
 
 ex2 :: (a -> Bool) -> [a] -> [a]
 ex2 _ [] = []
 ex2 f l = replaceWhen f l ((reverse . (filter f)) l1)
     where l1 = (p2 . dup) l
-
 
 -- pointfree 
 ex2f f = (uncurry(replaceWhen f)) . (split id (reverse . (filter f)))
--- ex4.hs
+++ ex4.hs
@@ -1,1 +1,36 @@
 import Probability
+import Cp
+import List
+import Data.List
+import Data.Maybe
+
+
+data Stop = S0 | S1 | S2 | S3 | S4 | S5 deriving (Show, Eq, Ord, Enum)
+type Segment = (Stop, Stop)
+
+-- dados :: [(Segment, Delay)]
+-- db :: [(Segment, Dist Delay)]
+-- mkdist :: Eq a ⇒ [a] → Dist a
+-- pdelay :: Stop → Stop → Dist Delay
+
+type Delay = Int
+type Dados = [(Segment, Delay)]
+type Db = [(Segment, Dist Delay)]
+
+-- mkDist :: Eq a => [a] -> Dist a
+-- mkDist = uniform --faz a distribuição uniforme dos dados
+
+dados :: [(Segment, Delay)]
+dados = [((S0, S1), 0), ((S0, S1), 2), ((S0, S1), 0), ((S0, S1), 3), ((S0, S1), 3),
+         ((S1, S2), 0), ((S1, S2), 2), ((S1, S2), 1), ((S1, S2), 1), ((S1, S2), 4),
+         ((S2, S3), 2), ((S2, S3), 2), ((S2, S3), 4), ((S2, S3), 0), ((S2, S3), 5),
+         ((S3, S4), 2), ((S3, S4), 3), ((S3, S4), 5), ((S3, S4), 2), ((S3, S4), 0),
+         ((S4, S5), 0), ((S4, S5), 5), ((S4, S5), 0), ((S4, S5), 7), ((S4, S5), -1)]
+
+mkDB :: Eq a => [(a, b)] -> [(a, Dist b)]
+mkDB = map (split (p1 . head) (uniform . map p2)) . groupBy (\x y -> p1 x == p1 y)
+
+hashT = mkDB dados 
+
+delay :: Segment -> Dist Delay
+delay = fromJust . uncurry(List.lookup) . (split id conshashT)  
